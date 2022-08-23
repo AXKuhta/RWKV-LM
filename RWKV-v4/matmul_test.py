d000d73550f22ba0d7821f22e0ecc08615761170
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-08-23 04:30:21 +0000

    Matmul test
    Doesn't work on webgl backend

---
+++ RWKV-v4/matmul_test.py
@@ -0,0 +1,18 @@
+import torch
+
+class MATMUL(torch.nn.Module):
+	def __init__(self):
+		super().__init__()
+
+	def forward(self, mat, vec):
+		return mat @ vec
+
+def export():
+	model = MATMUL()
+
+	mat = torch.rand([768, 768])
+	vec = torch.rand([768])
+
+	torch.onnx.export(model, args=(mat, vec), f="matmul.onnx", input_names = ["mat", "vec"], output_names = ["x"], verbose=True)
+
+
